d16b11a8c605eb04644d30b9e7059f6b6477a4ab
Kleadron <kleadron04@gmail.com>
2025-01-24 04:17:33 -0800

    Change registry keys from CroTeam to KSoft

--- Sources/WorldEditor/WorldEditor.cpp
+++ Sources/WorldEditor/WorldEditor.cpp
@@ -560,9 +560,9 @@ BOOL CWorldEditorApp::SubInitInstance()
 
   // settings will be saved into registry instead of ini file
   if (_strModExt=="") {
-    SetRegistryKey( CString("CroTeam"));
+    SetRegistryKey( CString("KSoft"));
   } else {
-    SetRegistryKey( CString("CroTeam\\"+_strModExt));
+    SetRegistryKey( CString("KSoft\\"+_strModExt));
   }
 
   CPrintF("%s", cmd_strOutput);
